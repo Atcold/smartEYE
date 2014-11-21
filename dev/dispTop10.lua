bcca63ff400aec67ef24b484c38da4b3dd3a7139
Alfredo Canziani <alfredo.canziani@gmail.com>
2014-11-21 12:02:51 -0500

    DEV - Added commented saving to dispTop10

--- dev/dispTop10.lua
+++ dev/dispTop10.lua
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- Display the top10 test images
+-- Display (or save) the top10 test images
 --------------------------------------------------------------------------------
 -- Alfredo Canziani, Nov 14
 --------------------------------------------------------------------------------
@@ -8,5 +8,11 @@ require 'image'
 data = torch.load('Top10TestData.t7')
 
 for label, cls in pairs(data) do
+   -- Display
    image.display{image = cls.image, legend = label, nrow = 5}
+
+   -- Save
+   -- image.saveJPG(label .. '.jpg',image.toDisplayTensor{
+   --    input = cls.image, legend = label, nrow = 5
+   -- })
 end
