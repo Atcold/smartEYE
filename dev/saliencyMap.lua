cef2c6c4a284c8355dcd0f37416cfaeb2adbfeff
Alfredo Canziani <alfredo.canziani@gmail.com>
2015-01-23 19:35:05 -0500

    DEV - Added profiling to <saliencyMap>

--- dev/saliencyMap.lua
+++ dev/saliencyMap.lua
@@ -6,21 +6,56 @@
 
 require 'image'
 require 'getModel'
+--require 'getSpatialModel' -- remove preprocessing for fairness
 
-for label, class in pairs(top10) do
-   io.write('Processing ' .. label); io.flush()
-   for n, img in ipairs(class.image) do
-      gradLoss = loss:updateGradInput(model:forward(img:cuda()), revClas[label])
-      gradInput = model:updateGradInput(img:cuda(), gradLoss)
-      image.display{
-         image = {
-            img,
-            gradInput,
-            gradInput:clone():max(1):repeatTensor(3,1,1),
-            gradInput:clone():abs():max(1):repeatTensor(3,1,1)
-         }, scaleeach = true, nrow = 2, zoom = 2, legend = label..'['..n..']'
-      }
+io.write('[V]iew or [P]rofile? (V/P) ')
+userInput = io.read()
+if userInput == 'V' or userInput == 'v' then
+   for label, class in pairs(top10) do
+      io.write('Processing ' .. label); io.flush()
+      for n, img in ipairs(class.image) do
+         gradLoss = loss:updateGradInput(model:forward(img:cuda()), revClas[label])
+         gradInput = model:updateGradInput(img:cuda(), gradLoss)
+         image.display{
+            image = {
+               img,
+               gradInput,
+               gradInput:clone():max(1):repeatTensor(3,1,1),
+               gradInput:clone():abs():max(1):repeatTensor(3,1,1)
+            }, scaleeach = true, nrow = 2, zoom = 2, legend = label..'['..n..']'
+         }
+      end
+      io.write(". Press enter for next category, 'q' for quitting... ")
+      if io.read() == 'q' then break end
    end
-   io.write(". Press enter for next category, 'q' for quitting... ")
-   if io.read() == 'q' then break end
+else
+   print('Profiling...')
+   count = 0
+   timer = torch.Timer()
+   for i = 1, 10 do
+      for label, class in pairs(top10) do
+         for n, img in ipairs(class.image) do
+            gradLoss = loss:updateGradInput(model:forward(img:cuda()), revClas[label])
+            --gradLoss = model:forward(img:cuda()) -- if getSpatialModel
+            count = count + 1
+         end
+      end
+   end
+   cutorch.synchronize()
+   forwardTime = timer:time().real / count
+   timer:reset()
+   for i = 1, 10 do
+      for label, class in pairs(top10) do
+         for n, img in ipairs(class.image) do
+            gradLoss = loss:updateGradInput(model:forward(img:cuda()), revClas[label])
+            --gradLoss = model:forward(img:cuda()) -- if getSpatialModel
+            gradInput = model:updateGradInput(img:cuda(), gradLoss)
+         end
+      end
+   end
+   cutorch.synchronize()
+   forwardAndGradTime = timer:time().real / count
+   print('Forward time          / sample [ms]: ', 1e3 * forwardTime)
+   print('Forward and grad time / sample [ms]: ', 1e3 * forwardAndGradTime)
+   print('Just grad time        / sample [ms]: ', 1e3 * (forwardAndGradTime - forwardTime))
 end
