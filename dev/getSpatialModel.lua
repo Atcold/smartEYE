4a9f5f78f8c88e80d9e06e9ca9847965abe12421
Alfredo Canziani <alfredo.canziani@gmail.com>
2015-06-15 16:27:42 -0400

    DEV - Support to Jetson TK1

--- dev/getSpatialModel.lua
+++ dev/getSpatialModel.lua
@@ -4,50 +4,60 @@
 -- Alfredo Canziani, Nov 14, Feb 15
 --------------------------------------------------------------------------------
 
--- Getting model and renaming
-require 'getModel'
+require 'sys'
 
-oldModel = model
-oldMLP = oldModel.modules[2]
+if sys.execute('uname -a'):find('tegra') then
+   require 'cudnn'
+   model = torch.load('../net/17cate9filter/model-127.net.tegra'):cuda()
+   SM = cudnn.SoftMax():cuda()
+else
 
--- Defining new MLP
-newMLP = nn.Sequential()
-newMLP:add(nn.SpatialConvolutionMM(  32, 2048, 6, 6))
-newMLP:add(nn.ReLU())
-newMLP:add(nn.SpatialConvolutionMM(2048, 2048, 1, 1))
-newMLP:add(nn.ReLU())
-newMLP:add(nn.SpatialConvolutionMM(2048,   17, 1, 1))
+   -- Getting model and renaming
+   require 'getModel'
 
--- Sending MLP to Cuda
-newMLP:cuda()
+   oldModel = model
+   oldMLP = oldModel.modules[2]
 
--- Copying over the weights from oldMLP
--- Kernels
-newMLP.modules[1].weight:copy(oldMLP.modules[2].weight)
-newMLP.modules[3].weight:copy(oldMLP.modules[4].weight)
-newMLP.modules[5].weight:copy(oldMLP.modules[6].weight)
--- Bias
-newMLP.modules[1].bias:copy(oldMLP.modules[2].bias)
-newMLP.modules[3].bias:copy(oldMLP.modules[4].bias)
-newMLP.modules[5].bias:copy(oldMLP.modules[6].bias)
+   -- Defining new MLP
+   newMLP = nn.Sequential()
+   newMLP:add(nn.SpatialConvolutionMM(  32, 2048, 6, 6))
+   newMLP:add(nn.ReLU())
+   newMLP:add(nn.SpatialConvolutionMM(2048, 2048, 1, 1))
+   newMLP:add(nn.ReLU())
+   newMLP:add(nn.SpatialConvolutionMM(2048,   17, 1, 1))
 
--- Creating new final model WITH preprocessing
-model = nn.Sequential():cuda()
-model:add(nn.AddConstant(-0.4124)):cuda()
-model:add(nn.MulConstant(1/0.2805)):cuda()
-model:add(oldModel.modules[1])
-model:add(newMLP)
+   -- Sending MLP to Cuda
+   newMLP:cuda()
+
+   -- Copying over the weights from oldMLP
+   -- Kernels
+   newMLP.modules[1].weight:copy(oldMLP.modules[2].weight)
+   newMLP.modules[3].weight:copy(oldMLP.modules[4].weight)
+   newMLP.modules[5].weight:copy(oldMLP.modules[6].weight)
+   -- Bias
+   newMLP.modules[1].bias:copy(oldMLP.modules[2].bias)
+   newMLP.modules[3].bias:copy(oldMLP.modules[4].bias)
+   newMLP.modules[5].bias:copy(oldMLP.modules[6].bias)
+
+   -- Creating new final model WITH preprocessing
+   model = nn.Sequential():cuda()
+   model:add(nn.AddConstant(-0.4124)):cuda()
+   model:add(nn.MulConstant(1/0.2805)):cuda()
+   model:add(oldModel.modules[1])
+   model:add(newMLP)
+
+   -- Cleaning
+   oldModel = nil
+   oldMLP = nil
+   collectgarbage()
+
+end
 
 -- Creating a SpatialSoftMax function
 nn.SpatialSoftMax = function (spatialLogit)
    return SM(
       spatialLogit
          :reshape(spatialLogit:size(1), spatialLogit:size(2) * spatialLogit:size(3))
-         :t()
+         :t():contiguous()
       ):t():reshape(spatialLogit:size())
 end
-
--- Cleaning
-oldModel = nil
-oldMLP = nil
-collectgarbage()
